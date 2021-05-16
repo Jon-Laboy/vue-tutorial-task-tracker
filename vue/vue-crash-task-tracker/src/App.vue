422f660d442c52b4caae991539402da23720442f
Jon Laboy <jonlaboyoc@gmail.com>
2021-05-16 10:17:49 -0400

    backend json working

--- vue/vue-crash-task-tracker/src/App.vue
+++ vue/vue-crash-task-tracker/src/App.vue
@@ -48,17 +48,35 @@ export default {
         body: JSON.stringify(task),
       });
 
-      const data = await res.json();
+      const data = await response.json();
       this.tasks = [...this.tasks, data];
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm("Are you sure?")) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: "DELETE",
+        });
+        response.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      });
+      const data = await response.json();
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     async fetchTasks() {
@@ -70,7 +88,7 @@ export default {
         console.log(`couldnt fetch : ${err}`);
       }
     },
-    async fetchTask() {
+    async fetchTask(id) {
       try {
         const response = await fetch(`http://localhost:5000/tasks/${id}`);
         const data = await response.json();
